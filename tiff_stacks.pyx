3f6c6a8b3c9403b75de5d64add5f4eb7ad06514d
Corey Smith <coreys@uchicago.edu>
2018-04-11 15:33:56 -0500

    remove debug prints and comments

--- tiff_stacks.pyx
+++ tiff_stacks.pyx
@@ -12,10 +12,6 @@ cdef extern from "tiff_io.h":
     int WriteTiffStacks(float* img_array, int* shape, const char* filename);
 
     int TiffInfo(const char* filename, int* shape);
-#
-#    int sim_decon(float* estimate, float* raw_data, float* illum_grid,
-#                  float* kernel, float* sens, float* shape, int num_images)
-    
     
 def read_stack(filename):
     ''' this is a ...
@@ -42,15 +38,12 @@ def write_stack_function(np.ndarray[float, ndim=3, mode="c"] img_stack, filename
         np.ndarray[int, ndim=1, mode='c'] sshape
         np.ndarray[float, ndim=1, mode="c"] Shape = np.empty([3], dtype=np.float32) 
 
-     
-    print(img_stack.shape[0], img_stack.shape[1], img_stack.shape[2])
     img_stack = np.ascontiguousarray(np.swapaxes(img_stack, 0, 2), 
                                          dtype=np.float32)
     Shape[0] = int(img_stack.shape[0])
     Shape[1] = int(img_stack.shape[1])
     Shape[2] = int(img_stack.shape[2])       
     sshape = np.ascontiguousarray(Shape, dtype=ctypes.c_int)
-    print(sshape)
     WriteTiffStacks(&img_stack[0,0,0], &sshape[0], c_filename)
 
 def write_stacks(img_stack, filename):
@@ -61,45 +54,6 @@ def write_stacks(img_stack, filename):
     filename = str(filename)
     
     write_stack_function(new_img, filename)
-    
-    
-    
-    
-#
-#def SIM_RL_Decon(np.ndarray[float, ndim=3, mode="c"] img_stack,
-#                  np.ndarray[float, ndim=3, mode="c"] grid_stack,
-#                  np.ndarray[float, ndim=2, mode="c"] kernel,
-#                  np.ndarray[float, ndim=2, mode="c"] sens):
-#    cdef:
-#        np.ndarray[float, ndim=1, mode="c"] Shape = np.empty([2], dtype=np.float32)
-#        np.ndarray[float, ndim=2, mode="c"] estimate
-#        int num_images
-#    
-#    img_stack = np.ascontiguousarray(np.swapaxes(img_stack, 0, 2), dtype=np.float32)
-#    #grid_stack = np.ascontiguousarray(np.swapaxes(grid_stack, 0, 2), dtype=np.float32)
-#
-#    num_images = img_stack.shape[0]
-#    Shape[0] = int(img_stack.shape[1])
-#    Shape[1] = int(img_stack.shape[2])
-#
-#    estimate = np.ones(Shape, dtype = np.float32) * 0.5
-#
-#    sim_decon(&estimate[0,0], &img_stack[0,0,0], &grid_stack[0,0,0],
-#              &kernel[0,0], &sens[0,0], &Shape[0], num_images)
-#
-#    img_stack = np.ascontiguousarray(np.swapaxes(img_stack, 0, 2), dtype=np.float32)
-#    estimate = np.ascontiguousarray(np.swapaxes(estimate, 0, 1), dtype=np.float32)
-#
-#    return estimate
-
-
-
-
-
-
-
-
-
 
 
 
